a34650556cad23ceec226040d66617d3349027c4
rurinaa <yuewall2013@naver.com>
2022-11-07 14:48:33 +0900

    소스코드 first_code에 calc_area 함수 추가

--- real_first_code.py
+++ real_first_code.py
@@ -1,2 +1,11 @@
+from re import X
+from tkinter import Y
+from unittest import result
+
+
 print("hello worid")
 print("This is the real first code")
+
+def calc_area(x, y):
+ result = x * y
+ return result
